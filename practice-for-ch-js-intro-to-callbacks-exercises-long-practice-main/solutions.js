b7601cc51f43498fa79b911bd001ea7ade8c5edb
DingoDang1113 <Shalpeng109@gmail.com>
2023-07-12 17:27:24 -0700

    more js stuff

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/solutions.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/solutions.js
@@ -5,8 +5,8 @@ class Clock {
         this.hours = time.getHours();
         this.mins = time.getMinutes();
         this.secs = time.getSeconds();
-       // setInterval(this._tick.bind(this), 1000);
-        setInterval(()=>this._tick(), 1000);
+        setInterval(this._tick.bind(this), 1000);
+        // setInterval(()=>this._tick(), 1000);
       // 1. Create a Date object.
       // 2. Store the hours, minutes, and seconds.
       // 3. Call printTime.
@@ -40,8 +40,64 @@ class Clock {
     }
 }
   
-  const clock = new Clock();
+  // const clock = new Clock();
 //console.log(clock)
-clock.printTime();
+  // clock.printTime();
   /*let today = new Date
   console.log(today)*/
+
+  // ****addNumbers ****** 
+  const reader = require('readline');
+  const rl = reader.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  function addNumbers(sum, numsLeft, completionCallback) {
+
+    if (numsLeft === 0) {
+    return completionCallback(sum);
+  }
+
+    if (numsLeft > 0) {
+      rl.question("give me a number", res => {
+        // debugger
+        sum += parseInt(res);
+        console.log(`the sum is ${sum}`);
+        addNumbers(sum, numsLeft - 1 , completionCallback)
+      });
+ 
+  };
+
+}
+
+  addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`)) // 3 numbers, partial sum, total sum
+
+
+// myBind //
+
+Function.prototype.myBind = function(context) {
+  return () => {this.apply(context, [])}
+};
+
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+  console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
+
